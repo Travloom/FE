f4708dd7fbb681fbfef7dd91bec7136da7207999
jjjuni <0210leejun@gmail.com>
2025-04-28 14:07:05 +0900

    💄 헤더 UI 구현

---
+++ src/component/layout/Header.tsx
@@ -0,0 +1,21 @@
+import { KakaoIcon } from "../../../public/svgs";
+
+interface HeaderProps {
+  title?: string
+}
+
+const Header:React.FC<HeaderProps> = ({
+  title,
+}) => {
+
+  return (
+    <div className={`w-full px-[60px] h-[100px] flex items-center justify-between bg-[rgba(108,92,231,0.15)] text-[40px] border-b border-b-gray-300 shadow-[0_4px_10px_0_rgba(0,0,0,0.1)]`}>
+      <p className={`mt-2`}>떠나,봄</p>
+      {title &&
+      <p className={`mt-2`}>{title}</p>}
+      <KakaoIcon className={`w-12`}/>
+    </div>
+  )
+}
+
+export default Header;
