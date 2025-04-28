b647beafbab9da845b8e160ab7edb0adc9476034
jjjuni <0210leejun@gmail.com>
2025-04-28 13:28:00 +0900

    ➕ storybook 의존성 추가 및 설정

---
+++ .storybook/main.ts
@@ -0,0 +1,45 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs",
+    "options": {
+      fastRefresh: true
+    }
+  },
+  "staticDirs": [
+    "..\\public"
+  ],
+  webpackFinal: async config => {
+    const imageRule = config.module?.rules?.find(rule => {
+      const test = (rule as { test: RegExp }).test
+
+      if (!test) {
+        return false
+      }
+
+      return test.test('.svg')
+    }) as { [key: string]: any }
+
+    imageRule.exclude = /\.svg$/
+
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    })
+
+    return config
+  }
+
+};
+export default config;
