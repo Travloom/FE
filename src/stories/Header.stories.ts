2f75d7969508de0c7007e5e26a480a4de0fdf6cd
jjjuni <0210leejun@gmail.com>
2025-04-28 14:07:23 +0900

    💄 헤더 story 생성

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -1,33 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
 
-import { Header } from './Header';
+import Header from '@/component/layout/Header';
 
 const meta = {
   title: 'Example/Header',
   component: Header,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
   },
   args: {
-    onLogin: fn(),
-    onLogout: fn(),
-    onCreateAccount: fn(),
+    title: ""
   },
 } satisfies Meta<typeof Header>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const LoggedIn: Story = {
+export const LoggedOutHome: Story = {};
+export const LoggedOutMyPage: Story = {
   args: {
-    user: {
-      name: 'Jane Doe',
-    },
-  },
+    title: "마이페이지"
+  }
 };
-
-export const LoggedOut: Story = {};
