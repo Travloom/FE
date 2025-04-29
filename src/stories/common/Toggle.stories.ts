06db7ce5b6676129f24d1a26aaaf9d1a8983fc89
jjjuni <0210leejun@gmail.com>
2025-04-29 10:05:55 +0900

    💄 토글 story 생성

---
+++ src/stories/common/Toggle.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Toggle from '@/component/common/Toggle';
+
+const meta = {
+  title: 'Common/Toggle',
+  component: Toggle,
+  tags: ['autodocs'],
+  parameters: {
+  },
+  args: {
+    text: "식당",
+    isActive: true,
+  },
+} satisfies Meta<typeof Toggle>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Active: Story = {
+  args: {
+    text: "식당",
+    isActive: true,
+  }
+};
+
+export const Inactive: Story = {
+  args: {
+    text: "식당",
+    isActive: false,
+  }
+};
