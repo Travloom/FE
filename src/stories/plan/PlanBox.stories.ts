e3bc7f98a9f67f643076f397fcb04829c6826419
jjjuni <0210leejun@gmail.com>
2025-04-30 15:24:10 +0900

    💄 PlanBox 및 Planner story 생성

---
+++ src/stories/plan/PlanBox.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import PlanBox from '@/component/plan/PlanBox';
+
+const meta = {
+  title: 'Plan/PlanBox',
+  component: PlanBox,
+  tags: ['autodocs'],
+  parameters: {
+  },
+  args: {
+    title: "출발~",
+    content: "간단하게 아점먹고 숙소로 출발~",
+  },
+} satisfies Meta<typeof PlanBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Share: Story = {
+  args: {
+    title: "출발~",
+    content: "간단하게 아점먹고 숙소로 출발~",
+  }
+};
---
+++ src/stories/plan/Planner.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Planner from '@/component/plan/Planner';
+
+const meta = {
+  title: 'Plan/Planner',
+  component: Planner,
+  tags: ['autodocs'],
+  parameters: {
+  },
+  args: {
+  },
+} satisfies Meta<typeof Planner>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Share: Story = {
+  args: {
+  }
+};
