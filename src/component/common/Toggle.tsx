a3fa4e6e23cc9092899ae46a5c4c034b434e3f4b
jjjuni <0210leejun@gmail.com>
2025-05-12 17:18:20 +0900

    ✨ 토글 선택 기능 추가

--- src/component/common/Toggle.tsx
+++ src/component/common/Toggle.tsx
@@ -1,21 +1,26 @@
+import { Dispatch, SetStateAction } from "react";
+
 interface ToggleProps{
   text: string;
   isActive: boolean;
+  setSelectedPlace: Dispatch<SetStateAction<string>>;
 }
 
 const Toggle:React.FC<ToggleProps> = ({
   text,
   isActive,
+  setSelectedPlace,
 }) => {
 
   const activeStyle = `text-white border border-point bg-point`
   const inactiveStyle = `text-point border border-point bg-white`
 
   return (
-    <div 
+    <div
       className={`
         ${isActive ? activeStyle : inactiveStyle} 
-        text-[20px] rounded-[8px] w-[120px] h-[44px] flex items-center justify-center cursor-pointer transition-all-300-out`}>
+        text-[20px] rounded-[8px] w-[120px] h-[44px] flex items-center justify-center cursor-pointer transition-all-300-out`}
+      onClick={() => setSelectedPlace(text)}>
       <p className={`mt-0.5`}>{text}</p>
     </div>
   )
