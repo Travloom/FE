e136870eaaeff8ecaef8e094bfca094358726a11
jjjuni <0210leejun@gmail.com>
2025-05-12 11:11:30 +0900

    💄 MapContent story 생성

---
+++ src/stories/plan/map/MapContent.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import MapContent from '@/component/plan/map/MapContent';
+
+const meta = {
+  title: 'Plan/Map/MapContent',
+  component: MapContent,
+  tags: ['autodocs'],
+  parameters: {
+  },
+  args: {
+  },
+} satisfies Meta<typeof MapContent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  args: {
+  }
+};
