b1d0547cb72848a00f06adf12cb2402331ad4fb1
jjjuni <0210leejun@gmail.com>
2025-05-13 10:05:02 +0900

    💄 global style 수정

--- src/component/layout/Header.tsx
+++ src/component/layout/Header.tsx
@@ -9,10 +9,10 @@ const Header:React.FC<HeaderProps> = ({
 }) => {
 
   return (
-    <div className={`w-full px-[60px] h-[100px] flex items-center justify-between bg-[rgba(108,92,231,0.15)] text-[40px] border-b border-b-gray-300 shadow-[0_4px_10px_0_rgba(0,0,0,0.1)]`}>
-      <p className={`mt-[1.5%] text-point`}>떠나,봄</p>
+    <div className={`absolute w-full px-[60px] h-[100px] flex items-center justify-between bg-[rgba(108,92,231,0.15)] text-[40px] border-b border-b-gray-300 shadow-[0_4px_10px_0_rgba(0,0,0,0.1)]`}>
+      <p className={`mt-[1%] text-point`}>떠나,봄</p>
       {title &&
-      <p className={`mt-[1.5%]`}>{title}</p>}
+      <p className={`mt-[1%]`}>{title}</p>}
       <KakaoIcon className={`w-12`}/>
     </div>
   )
