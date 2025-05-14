14d61eb853f73fd19074b5a546d559c75a497c00
jjjuni <0210leejun@gmail.com>
2025-05-14 09:47:37 +0900

    💄 style 수정

--- src/components/plan/Planner.tsx
+++ src/components/plan/Planner.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useState } from "react";
+import { useState } from "react";
 import Toggle from "../common/Toggle";
 import Scheduler from "./schedule/Scheduler";
 import PlaceList from "./map/PlaceList";
@@ -10,8 +10,8 @@ const Planner = () => {
   const [selectedToggle, setSelectedToggle] = useState("일정");
 
   return (
-    <div>
-      <div className={`flex flex-row gap-2.5 p-2.5 w-full border-b border-gray-300`}>
+    <div className={`border border-gray-300 rounded-[8px] bg-white h-full overflow-hidden`}>
+      <div className={`flex flex-row gap-2.5 p-2.5 w-full border-b border-gray-300 grow shrink-0`}>
         <Toggle text={"일정"} isActive={selectedToggle === "일정"} setSelectedToggle={setSelectedToggle}/>
         <Toggle text={"지도"} isActive={selectedToggle === "지도"} setSelectedToggle={setSelectedToggle}/>
       </div>
@@ -19,7 +19,7 @@ const Planner = () => {
         {selectedToggle === "일정" ? (
           <Scheduler/>
         ) : (
-          <div className={`flex flex-row`}>
+          <div className={`flex flex-row h-[540px]`}>
             <PlaceList restaurants={PlaceItems.restaurants} hotels={PlaceItems.hotels} attractions={PlaceItems.attractions}/>
             <MapContent/>
           </div>
