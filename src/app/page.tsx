b5162d37c8eddadbf88691b81b2bb9898f4ebd3a
jjjuni <0210leejun@gmail.com>
2025-05-15 08:49:31 +0900

    💫 animate 컴포넌트 생성 및 리팩토링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@
 import PlanInput from "@/components/common/PlanInput";
 import TagButton from "@/components/common/TagButton";
 import { TAGLIST } from "@/constants/Tag";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import { useEffect } from "react";
 import usePageStore from "../stores/usePageStore";
 import usePageAnimateRouter from "@/hooks/usePageAnimateRouter";
+import Motion from "@/components/motion/Motion";
 
 export default function Home() {
 
@@ -25,11 +26,7 @@ export default function Home() {
     <>
       <AnimatePresence>
         {!isPagePending &&
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
+          <Motion.MotionDiv
             className={`w-full h-full p-[60px] pt-[30%]`}>
             <div className={`flex flex-col gap-[40px] pb-[20px] h-full`}>
               <div className={`flex flex-col gap-5 w-full justify-center items-center`}>
@@ -49,7 +46,7 @@ export default function Home() {
                 className={`self-center text-point text-[20px] cursor-pointer pb-[20px]`}
                 onClick={() => pageAnimateRouter.push("/1")}>추천 없이 떠나기</p>
             </div>
-          </motion.div>
+          </Motion.MotionDiv>
         }
       </AnimatePresence>
     </>
