8f1a2bd2c3e21c965cd578b36af27157d08b9f70
jjjuni <0210leejun@gmail.com>
2025-05-20 14:24:42 +0900

    🐛 라우팅 버그 수정

--- src/hooks/common/usePageAnimateRouter.ts
+++ src/hooks/common/usePageAnimateRouter.ts
@@ -1,19 +1,22 @@
 import usePageStore from "@/stores/usePageStore";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const usePageAnimateRouter = () => {
 
   const router = useRouter();
+  const pathName = usePathname();
 
   const {
     setIsPagePending,
   } = usePageStore();
 
   const push = (url: string) => {
-    setIsPagePending(true)
-    setTimeout(() => {
-      router.push(url)
-    }, 300);
+    if (pathName !== url){
+      setIsPagePending(true)
+      setTimeout(() => {
+        router.push(url)
+      }, 300);
+    }
   }
 
   return { push }
