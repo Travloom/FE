ab1ce3504b98efb0a158638e05b6da580fd42915
jjjuni <0210leejun@gmail.com>
2025-05-27 16:30:33 +0900

    💄 아이콘 및 이미지 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -2,3 +2,12 @@ export { default as KakaoIcon } from "./kakao_icon.svg";
 export { default as PlaneIcon } from "./plane_icon.svg";
 export { default as RightArrowIcon } from "./right_arrow_icon.svg";
 export { default as MenuIcon } from "./menu_icon.svg";
+export { default as SearchIcon } from "./search_icon.svg";
+export { default as GoogleMapIcon } from "./google_map_icon.svg";
+
+export { default as MyRestaurantOnIcon } from "./my_restaurant_on_icon.svg";
+export { default as MyRestaurantOffIcon } from "./my_restaurant_off_icon.svg";
+export { default as MyHotelOnIcon } from "./my_hotel_on_icon.svg";
+export { default as MyHotelOffIcon } from "./my_hotel_off_icon.svg";
+export { default as MyAttractionOnIcon } from "./my_attraction_on_icon.svg";
+export { default as MyAttractionOffIcon } from "./my_attraction_off_icon.svg";
