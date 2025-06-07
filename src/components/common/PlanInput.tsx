5a90e2ab6fdaca3f1fdfab6a65c5e7fe435d5287
jjjuni <0210leejun@gmail.com>
2025-06-07 18:28:25 +0900

    💄 플랜 생성 로딩 UI 추가

--- src/components/common/PlanInput.tsx
+++ src/components/common/PlanInput.tsx
@@ -6,6 +6,9 @@ import { planRecommendRequest } from "@/apis/plan";
 import useHomeStore from "@/stores/useHomeStore";
 import { useMutation } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import Motion from "../motion/Motion";
+import { HashLoader } from "react-spinners";
+import { AnimatePresence } from "framer-motion";
 
 const PlanInput = () => {
 
@@ -53,31 +56,44 @@ const PlanInput = () => {
   });
 
   return (
-    <div
-      className={`
+    <>
+      <div
+        className={`
       lg:px-6 lg:py-4.5
       px-5 py-3.5 flex justify-between gap-2.5 rounded-full w-full  bg-white border border-gray-300 overflow-hidden transition-all-300-out`}>
-      <input
-        className={`
+        <input
+          className={`
           lg:text-[20px] 
           w-full text-[16px] font-light text-gray-700 placeholder:text-gray-300 grow transition-all-300-out`}
-        placeholder="여행 플랜 이름을 입력해주세요"
-        value={title || ``}
-        onChange={(e) => setTitle(e.target.value)} />
-      <PlaneIcon
-        className={`
+          placeholder="여행 플랜 이름을 입력해주세요"
+          value={title || ``}
+          onChange={(e) => setTitle(e.target.value)} />
+        <PlaneIcon
+          className={`
           ${(title?.trim() !== "" && isAllTagSelected && title && startDate && endDate) ? `text-point` : `text-gray-300`}
           lg:w-6 w-4 cursor-pointer transition-all-300-out`}
-        onClick={() => {
-          if (title?.trim() !== "" && isAllTagSelected && startDate && endDate) {
-            mutate();
+          onClick={() => {
+            if (title?.trim() !== "" && isAllTagSelected && startDate && endDate) {
+              mutate();
+            }
+          }} />
+      </div>
+      {
+        <AnimatePresence>
+          {isPending &&
+            <Motion.MotionDiv className={`absolute top-0 left-0 w-screen h-screen bg-[rgba(255,255,255,0.6)] z-50`}>
+              <div className={`flex flex-col gap-6 justify-center items-center w-full h-full`}>
+                <HashLoader
+                  size={30}
+                  color={`#6c5ce7`} />
+                  <p className={`lg:text-[16px] text-[14px] text-point`}>플랜 생성 중</p>
+              </div>
+
+            </Motion.MotionDiv>
           }
-        }} />
-        {isPending &&
-        <div className={`absolute w-screen h-screen bg-[rgba(1,1,1,0.5)]`}>
-        </div>
-        }
-    </div>
+        </AnimatePresence>
+      }
+    </>
   )
 }
 
