d4ce80966a01fbf92d6dcfcafb515b429ae9ad0c
jjjuni <0210leejun@gmail.com>
2025-06-10 16:49:59 +0900

    ✨ accessToken 재발급 인터셉터 추가

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -4,4 +4,26 @@ const axiosInstance = axios.create({
   withCredentials: true,
 })
 
+axiosInstance.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      try {
+        const response = await axios.post(
+          "/proxy/api/auth/refresh",
+          {},
+          { withCredentials: true }
+        );
+        
+        return axiosInstance(originalRequest);
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+)
+
+
 export default axiosInstance;
