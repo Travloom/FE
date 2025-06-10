9b82305d2995831722a0ef7ef0be09f6b4070002
jjjuni <0210leejun@gmail.com>
2025-06-10 16:51:59 +0900

    ✨ 추천 없이 플랜 생성

--- src/apis/plan.ts
+++ src/apis/plan.ts
@@ -10,6 +10,7 @@ interface PlanType {
   theme: string;
 }
 
+// 장소 추천받아 플랜 생성성
 export const planRecommendRequest = async (Plan: PlanType) => {
   try {
     const response = await axiosInstance.post(`/proxy/api/places`, {
@@ -20,6 +21,41 @@ export const planRecommendRequest = async (Plan: PlanType) => {
       companions: Plan.companions,
       people: Plan.people,
       theme: Plan.theme,
+    })
+    return response.data;
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+// 추천 없이 플랜 생성
+export const createPlanRequest = async (Plan: PlanType) => {
+  try {
+    const response = await axiosInstance.post(`/proxy/api/plan`, {
+      title: Plan.title,
+      startDate: Plan.startDate,
+      endDate: Plan.endDate,
+      region: Plan.region,
+      companions: Plan.companions,
+      people: Plan.people,
+      theme: Plan.theme,
+    })
+    return response.data;
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+// 플랜 목록 조회
+export const getPlansRequest = async ({before, after, year, month}: {before?: Date, after?: Date, year?: number, month?: number}) => {
+  try {
+    const response = await axiosInstance(`/proxy/api/plans`, {
+      params: {
+        before,
+        after,
+        year,
+        month,
+      }
     })
     return response.data;
   } catch (e) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,8 @@ import CustomDatePicker from "@/components/date-picker/CustomDatePicker";
 import useHomeStore from "@/stores/useHomeStore";
 import { TagsType } from "@/types/place/type";
 import { AnimatePresence } from "framer-motion";
+import { createPlanRequest } from "@/apis/plan";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
@@ -18,65 +20,98 @@ export default function Home() {
     isPagePending,
   } = usePageStore();
 
+
   const {
+    title,
     tags,
-    setTag
+    startDate,
+    endDate,
+    setTag,
+    setTitle,
   } = useHomeStore();
+
+  const [isAllTagSelected, setIsAllTagSelected] = useState(false);
+
+  useEffect(() => {
+    setIsAllTagSelected(Object.values(tags).every((tag) => !!tag));
+  }, [tags])
 
   const pageAnimateRouter = usePageAnimateRouter();
 
   useInitPage(null)
 
+  const createPlan = async () => {
+    if (isAllTagSelected && title && startDate && endDate) {
+      try {
+        const plan = await createPlanRequest({
+          title: title,
+          startDate: startDate,
+          endDate: endDate,
+          region: tags.region as string,
+          people: tags.people as string,
+          companions: tags.companions as string,
+          theme: tags.theme as string,
+        });
+
+        pageAnimateRouter.push(`/${plan.uuid}`)
+
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+
   return (
     <AnimatePresence>
-        {!isPagePending &&
-          <Motion.MotionDiv
-            className={`
+      {!isPagePending &&
+        <Motion.MotionDiv
+          className={`
               md:px-[20%] 
               w-full h-full px-[28px] pb-[60px] pt-[100px] transition-all-300-out`}>
-            <div className={`flex flex-col gap-[40px] pb-[20px] h-full justify-center`}>
-              <div
-                className={`
+          <div className={`flex flex-col gap-[40px] pb-[20px] h-full justify-center`}>
+            <div
+              className={`
                   lg:gap-5
                   gap-2.5 flex flex-col w-full justify-center items-center`}>
-                <p
-                  className={`
+              <p
+                className={`
                     lg:text-[40px]
                     text-point text-[28px] transition-all-300-out`}>
-                  어디로 떠나볼까요?
-                </p>
-                <div
-                  className={`
+                어디로 떠나볼까요?
+              </p>
+              <div
+                className={`
                     lg:gap-5 lg:max-w-[750px]
                     gap-2.5 w-full  flex flex-col transition-all-300-out`}>
-                  <PlanInput/>
-                  <div
-                    className={`
+                <PlanInput />
+                <div
+                  className={`
                       lg:max-h-[119px]
                       max-h-[108px] w-full flex flex-col gap-10`}>
-                    <div className={`flex flex-row gap-2.5 w-full top-0`}>
-                      <CustomDatePicker/>
-                      {Object.entries(TAGLIST).map(([key, tag]) => (
-                        <TagButton
-                          key={key}
-                          title={tag.title}
-                          tagList={tag.tagList}
-                          currentTag={tags[key as keyof TagsType] || tag.title}
-                          setCurrentTag={(value: string) => setTag(key as keyof TagsType, value)}
-                        />
-                      ))}
-                    </div>
-                    <p
-                      className={`
-                        lg:text-[20px] 
-                        text-[14px] self-center text-point cursor-pointer pb-[20px] transition-all-300-out`}
-                      onClick={() => pageAnimateRouter.push("/1")}>추천 없이 떠나기</p>
+                  <div className={`flex flex-row gap-2.5 w-full top-0`}>
+                    <CustomDatePicker />
+                    {Object.entries(TAGLIST).map(([key, tag]) => (
+                      <TagButton
+                        key={key}
+                        title={tag.title}
+                        tagList={tag.tagList}
+                        currentTag={tags[key as keyof TagsType] || tag.title}
+                        setCurrentTag={(value: string) => setTag(key as keyof TagsType, value)}
+                      />
+                    ))}
                   </div>
+                  <p
+                    className={`
+                      ${(isAllTagSelected && title && startDate && endDate) ? `text-point cursor-pointer` : `text-gray-200`}
+                        lg:text-[20px] 
+                        text-[14px] self-center pb-[20px] transition-all-300-out select-none`}
+                    onClick={createPlan}>추천 없이 떠나기</p>
                 </div>
               </div>
             </div>
-          </Motion.MotionDiv>
-        }
+          </div>
+        </Motion.MotionDiv>
+      }
     </AnimatePresence>
   );
 }
