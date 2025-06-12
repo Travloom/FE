9afa0fd69db20beffd416bdc949dec5e10c44052
jjjuni <0210leejun@gmail.com>
2025-06-12 11:31:15 +0900

    🔍 SEO 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,18 @@
+import Head from "next/head";
 import Providers from "./providers";
 import "@/styles/globals.css";
-
-export const metadata = {
-  title: '떠나,봄',
-  description: 'AI와 함께하는 스마트 여행 플래너, 떠나,봄',
-  icons: {
-    icon: '/svgs/favicon.svg',
-  },
-  openGraph: {
-    title: '떠나,봄',
-    description: 'AI와 함께하는 스마트 여행 플래너, 떠나,봄',
-    url: 'https://travloom.store',
-    siteName: '떠나,봄',
-    images: [
-      {
-        url: '/images/seo_image.png',
-        width: 1200,
-        height: 630,
-        alt: '떠나,봄 서비스 소개 이미지',
-      },
-    ],
-    locale: 'ko_KR',
-    type: 'website',
-  },
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <Head>
+        <title>떠나,봄</title>
+        <link rel="icon" href="/svgs/favicon.svg"/>
+        <meta name="description" content={"AI와 함께하는 스마트 여행 플래너, 떠나,봄"} />
+        <meta property="og:title" content={"떠나,봄봄"} />
+        <meta property="og:description" content={"AI와 함께하는 스마트 여행 플래너, 떠나,봄"} />
+        <meta property="og:image" content="/images/seo_image.png" />
+      </Head>
       <body>
         <Providers>{children}</Providers>
       </body>
